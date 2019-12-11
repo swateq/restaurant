05b51700c59ce7d21cfe17e7fe55e7948d4d8e8c
swateq <swat94@tlen.pl>
2019-12-11 11:34:51 +0100

    Ostatni news cacheowany

--- app/News.php
+++ app/News.php
@@ -4,15 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class News extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, QueryCacheable;
     
     protected $guarded = [''];
 
     public static function last()
     {
-        return News::where('active','1')->latest()->first();
+        return News::cacheFor(3000)->where('active','1')->latest()->first();
     }
 }
